793fe36f2e51843be8d1b7eea5c1992ee9b7853f
FOYSAL AHMED MiN <foysalahmedmin@gmail.com>
2023-03-10 21:27:29 +0600

    some game calculation complete

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,43 @@
 
 
 const ballAct = (className) => {
-    document.getElementById("ball").classList.add(className);
-    setTimeout(()=>{
-    document.getElementById("ball").classList.remove(className);
-    }, 500)
+    const ball = document.getElementById("ball");
+    if(!ball.classList.contains(className)){
+        ball.classList.add(className);
+        setTimeout(()=>{
+        ball.classList.remove(className);
+        }, 500)
+    }
+    
 
 }
 
-document.getElementById("gameField").onclick = () =>{
-    ballAct('ballAnimationJump');
-}
+document.onkeydown = (e) =>{
+
+    // e = e || window.event;
+
+    if (e.keyCode == '38') {
+        ballAct('ballAnimationJump');
+    }
+    else if (e.keyCode == '40') {
+        ballAct('ballAnimationDown');
+    }
+
+};
+
+let check = setInterval(() => {
+    const gameField = document.getElementById("gameField");
+    const ball = document.getElementById("ball");
+    const arrow = document.getElementById("arrow");
+    const ballJumpHeight = window.getComputedStyle(ball).getPropertyValue("bottom").slice(0, -2);
+    const ballDownHeight = window.getComputedStyle(ball).getPropertyValue("height").slice(0, -2);
+    const arrowPosition = window.getComputedStyle(arrow).getPropertyValue("left").slice(0, -2);
+
+    // console.log( 165 > +arrowPosition );
+    if(165 > arrowPosition && 100 < arrowPosition  && 90 < ballDownHeight){
+        // console.log("loss");
+        arrow.style.left = arrowPosition +"px"
+        arrow.style.animation = "none"
+        // gameField.innerHTML+= `<h1 style = "color: 'red'; position: 'absolute'; top:'47%'" >Game Over!</h1>`
+    }
+}, 100);
